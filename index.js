19421f8454e382bbfc060d460f014d3c695ab536
Aman Thapa <amansinghthapa33@gmail.com>
2025-07-29 20:39:36 +0530

    Applications API tested

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import connectdb from "./utils/db.js";
 import userRoute from "./routes/user.routes.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
 const PORT = process.env.PORT || 3000;
 dotenv.config({});
 
@@ -23,6 +24,7 @@ app.use(cors(corsOptions));
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
 app.listen(PORT, () => {
   connectdb();
