3a57c82d6406418b73bf412338c986e9cb9c7f39
Aman Thapa <amansinghthapa33@gmail.com>
2025-07-29 20:14:41 +0530

    Application routes completed

--- controllers/application.controller.js
+++ controllers/application.controller.js
@@ -65,3 +65,46 @@ export const getAppliedJobs = async (req, res) => {
     console.log(error);
   }
 };
+
+export const getApplicants = async (req, res) => {
+  try {
+    const jobId = req.params.id;
+    const job = await Job.findById(jobId).populate({
+      path: "application",
+      options: { sort: { createdAt: -1 } }.populate({ path: "applicant" }),
+    });
+    if (!job) {
+      return res.status(404).json({ message: "Job not found", success: false });
+    }
+    return res.status(200).json({ job, success: true });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateStatus = async (req, res) => {
+  try {
+    const { status } = req.body;
+    const applicationId = req.params.id;
+    if (!status) {
+      return res
+        .status(404)
+        .json({ message: "Status is required", success: false });
+    }
+    const application = await Application.findOne({ _id: applicationId });
+    if (!application) {
+      return res
+        .status(404)
+        .json({ message: "Application not found", success: true });
+    }
+
+    application.status = status.toLowerCase();
+    await application.save();
+
+    return res
+      .status(200)
+      .json({ message: "Status updated sucessfully", success: true });
+  } catch (error) {
+    console.log(error);
+  }
+};
